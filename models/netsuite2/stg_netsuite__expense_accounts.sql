481175fc1796efc3662d5bc258358e4d3dadc9ed
David Keeney <davidkeeney@place.com>
2022-06-22 09:15:34 -0700

    account updates

---
+++ models/netsuite2/stg_netsuite__expense_accounts.sql
@@ -0,0 +1,24 @@
+{{ config(enabled=var('data_model') == 'netsuite2') }}
+
+with base as (
+
+    select * 
+    from {{ ref('stg_netsuite__expense_accounts_tmp') }}
+
+),
+
+final as (
+    
+    select 
+        ID AccountID
+        ,ACCOUNTSEARCHDISPLAYNAME AccountName
+        ,ACCTNUMBER AccountNumber
+        ,ACCTTYPE AccountType
+        ,PARENT ParentID
+        ,_FIVETRAN_DELETED
+    from base
+)
+
+select * 
+from final
+where not coalesce(_fivetran_deleted, false)
---
+++ models/netsuite2/tmp/stg_netsuite__expense_accounts_tmp.sql
@@ -0,0 +1,5 @@
+{{ config(enabled=var('data_model') == 'netsuite2') }}
+
+select *
+from {{ var('accounts_netsuite2') }}
+where ACCTTYPE IN ('Expense', 'OthExpense')
