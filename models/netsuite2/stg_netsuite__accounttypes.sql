e047fd85628bec53dbc19e27f19bf51cc7c9608f
David Keeney <davidkeeney@place.com>
2022-06-22 16:20:06 -0700

    add account types staging table

---
+++ models/netsuite2/stg_netsuite__accounttypes.sql
@@ -0,0 +1,23 @@
+{{ config(enabled=var('data_model') == 'netsuite2') }}
+
+with base as (
+
+    select * 
+    from {{ ref('stg_netsuite__accounttypess_tmp') }}
+    where not coalesce(_fivetran_deleted, false)
+
+),
+
+final as (
+    
+    select
+        _fivetran_synced,
+        id as accounttype_id,
+        balancesheet = 'T' as is_balancesheet,
+        [left] = 'T' as is_leftside,
+        longname as [type_name]
+    from fields
+)
+
+select * 
+from final
