535a9821a22e34bb7a15a890c245ef2405b3caa7
David Keeney <davidkeeney@place.com>
2022-06-22 10:05:32 -0700

    VendorTypes

---
+++ models/netsuite2/stg_netsuite__vendor_types.sql
@@ -0,0 +1,22 @@
+{{ config(enabled=var('data_model') == 'netsuite') }}
+
+with base as (
+
+    select * 
+    from {{ ref('stg_netsuite__vendor_types_tmp') }}
+
+),
+
+final as (
+    
+    select 
+        ID vendor_type_id,
+        name,
+        _fivetran_deleted
+
+    from base
+)
+
+select * 
+from final
+where not coalesce(_fivetran_deleted, false)
